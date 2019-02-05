4b0e0e04dc9f5672b37a1936ad10d2fadaa31ede
10000daos10 <47340008+10000daos10@users.noreply.github.com>
2019-02-04 16:07:32 -0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,70 @@
+#python -m pip install discord.py==0.16.12
+#install above at administrative pc with command/shell
+
+# Work with Python 3.6
+import discord
+import random
+import asyncio
+import aiohttp
+import json
+from discord import Game
+from discord.ext.commands import Bot
+
+BOT_PREFIX = ("?", "!")
+TOKEN = 'XXXXXXXXXXXXXXXXXXXXXXXXX'
+
+client = Bot(command_prefix=BOT_PREFIX)
+
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.command(name='8ball',
+                description="Answers a yes/no question.",
+                brief="Answers from the beyond.",
+                aliases=['eight_ball', 'eightball', '8-ball'],
+                pass_context=True)
+async def eight_ball(context):
+    possible_responses = [
+        'That is a resounding no',
+        'It is not looking likely',
+        'Too hard to tell',
+        'It is quite possible',
+        'Definitely',
+    ]
+    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
+
+@client.command()
+async def square(number):
+    squared_value = int(number) * int(number)
+    await client.say(str(number) + " squared is " + str(squared_value))
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name="with humans"))
+    print("Logged in as " + client.user.name)
+
+async def list_servers():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        print("Current servers:")
+        for server in client.servers:
+            print(server.name)
+        await asyncio.sleep(600)
+
+client.loop.create_task(list_servers())
+client.run(TOKEN)
